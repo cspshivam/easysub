4c141940a0ffad164aa4a3656070a18a3e8d9add
whit3 h4t <59191514+cspshivam@users.noreply.github.com>
2020-11-24 16:19:25 +0530

    Add files via upload

---
+++ easysub.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+banner(){
+echo -e "\e[1;31m
+ ▄▄▄▄▄▄▄▄                                  ▄▄▄▄              ▄▄       
+ ██▀▀▀▀▀▀                                ▄█▀▀▀▀█             ██       
+ ██         ▄█████▄  ▄▄█████▄  ▀██  ███  ██▄       ██    ██  ██▄███▄  
+ ███████    ▀ ▄▄▄██  ██▄▄▄▄ ▀   ██▄ ██    ▀████▄   ██    ██  ██▀  ▀██ 
+ ██        ▄██▀▀▀██   ▀▀▀▀██▄    ████▀        ▀██  ██    ██  ██    ██ 
+ ██▄▄▄▄▄▄  ██▄▄▄███  █▄▄▄▄▄██     ███    █▄▄▄▄▄█▀  ██▄▄▄███  ███▄▄██▀ 
+ ▀▀▀▀▀▀▀▀   ▀▀▀▀ ▀▀   ▀▀▀▀▀▀      ██      ▀▀▀▀▀     ▀▀▀▀ ▀▀  ▀▀ ▀▀▀   
+                                ███                                   
+                                                                          \e[0m"
+echo -e "                          \e[95m by CSPSHIV\e[5mΔ\e[0m\e[95mM\e[0m"
+echo -e "                          \e[95m https://www.cspshivam.com \e[0m "
+echo -e "\e[31m--------------------------------------------------------------\e[0m"
+echo -e "\e[31m[-]Subdomain Enumeration Sources :-
++ crt
++ anubis
++ archive
++ certspotter
++ alienvault
++ dnsbufferover
++ dnsdumpster
++ hackertarget
++ rapiddns
++ riddler
++ threatcrowd
++ threatminer
++ urlscan
++ virustotal \e[0m"
+echo -e "\e[31m---------------------------------------------------------------\e[0m"
+}                                                                                                              
+usage(){
+        echo -e "\e[1;31m
+ ▄▄▄▄▄▄▄▄                                  ▄▄▄▄              ▄▄       
+ ██▀▀▀▀▀▀                                ▄█▀▀▀▀█             ██       
+ ██         ▄█████▄  ▄▄█████▄  ▀██  ███  ██▄       ██    ██  ██▄███▄  
+ ███████    ▀ ▄▄▄██  ██▄▄▄▄ ▀   ██▄ ██    ▀████▄   ██    ██  ██▀  ▀██ 
+ ██        ▄██▀▀▀██   ▀▀▀▀██▄    ████▀        ▀██  ██    ██  ██    ██ 
+ ██▄▄▄▄▄▄  ██▄▄▄███  █▄▄▄▄▄██     ███    █▄▄▄▄▄█▀  ██▄▄▄███  ███▄▄██▀ 
+ ▀▀▀▀▀▀▀▀   ▀▀▀▀ ▀▀   ▀▀▀▀▀▀      ██      ▀▀▀▀▀     ▀▀▀▀ ▀▀  ▀▀ ▀▀▀   
+                                ███                                   
+                                                                          \e[0m"
+echo -e "                          \e[95m by CSPSHIV\e[5mΔ\e[0m\e[95mM\e[0m"
+echo -e "                          \e[95m https://www.cspshivam.com \e[0m "
+echo "	"
+echo "	"
+	echo -e "\e[33m[Usage:] $ ./easysub.sh domain.com\e[0m"
+	echo -e "\e[33m FLAG : -------------------------------------------------------------\e[0m"
+	echo -e "\e[33m           -h : Help , Print This Message\e[0m"
+	echo -e "\e[33m           -s : Silent output - No banner, no progress, only domains\e[0m"
+}
+if [ $# -eq 0 ] || [ "$1" = "-h" ]
+  then
+    usage
+    exit
+fi
+if [ "$1" = "-s" ] && [ $# -eq 1 ]
+then
+    echo "Please Provide a domain!"
+    usage
+    exit
+fi
+
+silent=false
+if [ "$1" = "-s" ] && [ $# -eq 2 ]
+then
+    silent=true
+    set -- "$2" # $1=$2
+fi
+
+if [ "$silent" = "false" ]; then
+    banner
+fi
+
+    curl -s "https://otx.alienvault.com/api/v1/indicators/domain/$1/passive_dns" | jq -r ".passive_dns[].hostname" | sort -u > tmp.txt &
+    curl -s "https://jldc.me/anubis/subdomains/$1" | jq -r '.' | cut -d '"' -f2 | cut -d '[' -f1 | cut -d ']' -f1 | grep . | sort -u >> tmp.txt &
+    curl -s "http://web.archive.org/cdx/search/cdx?url=*.$1/*&output=text&fl=original&collapse=urlkey" | sort | sed -e 's_https*://__' -e "s/\/.*//" -e 's/:.*//' -e 's/^www\.//' | sort -u >> tmp.txt &
+    curl -s "https://certspotter.com/api/v0/certs?domain=$1" | jq '.[].dns_names[]' 2> /dev/null | sed 's/\"//g' | sed 's/\*\.//g' | grep -w $1\$ | sort -u >> tmp.txt &
+    curl -s "https://crt.sh/?q=%.$1&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u >> tmp.txt &
+    curl -s "https://dns.bufferover.run/dns?q=.$1" | jq -r .FDNS_A[] 2>/dev/null | cut -d ',' -f2 | grep -o "\w.*$1" | sort -u >> tmp.txt &
+    curl -s "https://dns.bufferover.run/dns?q=.$1" | jq -r .RDNS[] 2>/dev/null | cut -d ',' -f2 | grep -o "\w.*$1" | sort -u >> tmp.txt &
+    curl -s "https://tls.bufferover.run/dns?q=.$1" | jq -r .Results 2>/dev/null | cut -d ',' -f3 | grep -o "\w.*$1"| sort -u >> tmp.txt &
+    curl -s "https://api.hackertarget.com/hostsearch/?q=$1" | cut -d ',' -f1 | sort -u >> tmp.txt &
+    curl -s "https://rapiddns.io/subdomain/$1?full=1#result" | grep -oaEi "https?://[^\"\\'> ]+" | grep $1 | sed 's/https\?:\/\///' | cut -d "/" -f3 | sort -u >> tmp.txt &
+    curl -s "https://riddler.io/search/exportcsv?q=pld:$1" | grep -o "\w.*$1" | cut -d ',' -f6 | sort -u >> tmp.txt &
+    curl -s "https://www.threatcrowd.org/searchApi/v2/domain/report/?domain=$1" | jq '.subdomains' | cut -d '"' -f2 | cut -d '[' -f1 | cut -d ']' -f1 | grep . | sort -u >> tmp.txt &
+    curl -s "https://api.threatminer.org/v2/domain.php?q=$1&rt=5" | jq -r '.results[]' | sort -u >> tmp.txt &
+    curl -s "https://urlscan.io/api/v1/search/?q=domain:$1" | jq -r '.results[].page.domain' | sort -u >> tmp.txt &
+    curl -s "https://www.virustotal.com/ui/domains/$1/subdomains?limit=40" | grep '"id":' | cut -d '"' -f4 | sort -u >> tmp.txt &
+    csrftoken=$(curl -ILs https://dnsdumpster.com | grep csrftoken | cut -d " " -f2 | cut -d "=" -f2 | tr -d ";")
+    curl -s --header "Host:dnsdumpster.com" --referer https://dnsdumpster.com --user-agent "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0" --data "csrfmiddlewaretoken=$csrftoken&targetip=$1" --cookie "csrftoken=$csrftoken; _ga=GA1.2.1737013576.1458811829; _gat=1" https://dnsdumpster.com >> dnsdumpster.html
+    if [[ -e $1 && -s $1 ]]; then # file exists and is not zero size
+        cat dnsdumpster.html | grep "https://api.hackertarget.com/httpheaders" | grep -o "\w.*$1" | cut -d "/" -f7 | grep '.' | sort -u >> tmp.txt
+    fi
+    cat tmp.txt | grep -iv "*" | sort -u | grep $1 | tee
+    rm -rf dnsdumpster.html
+    rm -rf tmp.txt
+
+
+
+
